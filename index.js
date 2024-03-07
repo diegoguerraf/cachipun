5157cb253111a9abf7ccb0033768702fd0870885
diegoguerraf <guerra.farias.diego@gmail.com>
2024-03-06 21:46:27 -0300

    cambios mensaje rondas  finales

--- index.js
+++ index.js
@@ -20,8 +20,19 @@ function determinarGanador(jugadaUsuario, jugadaMaquina) {
 
 function jugarCachipun() {
     let continuar = true;
+    let totalJuegos = 0;
+    let victoriasUsuario = 0;
+    let victoriasComputadora = 0;
+    let empates = 0;
+
     while (continuar) {
+        victoriasUsuario = 0;
+        victoriasComputadora = 0;
+        empates = 0;
+        
         const veces = parseInt(prompt("¿Cuántas veces desea jugar?"));
+        totalJuegos += veces;
+        
         for (let i = 0; i < veces; i++) {
             const jugadaUsuario = prompt("Ingrese su jugada (piedra, papel o tijera):").trim().toLowerCase();
             
@@ -32,10 +43,13 @@ function jugarCachipun() {
                 let mensaje = `Partida ${i + 1}: Usted jugó ${jugadaUsuario}. La máquina jugó ${jugadaMaquina}. `;
                 if (resultado === "Usuario") {
                     mensaje += "¡Felicidades! Usted ha ganado esta partida.";
+                    victoriasUsuario++;
                 } else if (resultado === "Computadora") {
                     mensaje += "Lo siento, ha perdido contra la máquina.";
+                    victoriasComputadora++;
                 } else {
                     mensaje += "Ha sido un empate.";
+                    empates++;
                 }
                 alert(mensaje);
             } else {
@@ -43,7 +57,28 @@ function jugarCachipun() {
                 i--; 
             }
         }
+
+        alert(`Juegos completados. Total de victorias del usuario: ${victoriasUsuario}. Total de victorias de la computadora: ${victoriasComputadora}. Total de empates: ${empates}.`);
+
+        let mensajeFinal = "";
+        if (victoriasUsuario > victoriasComputadora) {
+            mensajeFinal = "¡Felicidades! Usted ha ganado más rondas en total.";
+        } else if (victoriasUsuario < victoriasComputadora) {
+            mensajeFinal = "Lo siento, ha perdido más rondas en total.";
+        } else {
+            mensajeFinal = "Sigue intentándolo, ha habido un empate en el total de rondas.";
+        }
+        
+        alert(mensajeFinal);
+        
+        const continuarJugando = confirm("¿Desea jugar nuevamente?");
+        if (!continuarJugando) {
+            continuar = false;
+        }
     }
 }
 
 jugarCachipun();
+
+
+
